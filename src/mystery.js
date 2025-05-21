c518ca3de9f1ba1892c578fc0592a051cca793e1
Mariya Mokrynska <mariyamokrynska1994@gmail.com>
2025-05-21 11:24:17 -0700

    done

--- src/mystery.js
+++ src/mystery.js
@@ -1,12 +1,11 @@
 export const mystery3 = (string1, string2) => {
-    if(string1.length === 0 && string2.length === 0){
+    if (string1.length === 0 && string2.length === 0) {
         return 0;
     }
     let counter = 0;
     for( let i = 0; i < string1.length; i++){
         if (string1[i]!== string2[i]){
-            counter++;
-        }
+            counter++;        
     }
     return counter;
 };    
